f52e4558d030f1a4d67b466ce672b6119f4b1f60
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-02-27 18:17:54 +0800

    aded images and auth page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24">
 
-      <iframe
+      {/* <iframe
         src={`https://api.instagram.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_ID}&redirect_uri=${'https://ig-planner-beta.vercel.app/'}&scope=${'user_profile,user_media'}&response_type=code`}
         allowFullScreen
-      />
+      /> */}
       <div className="grid grid-cols-3 gap-1">
-        <div className="bg-gray-700 size-32"></div>
-        <div className="bg-gray-700 size-32"></div>
-        <div className="bg-gray-700 size-32"></div>
+        <div className="bg-gray-700 size-32">
+          <img className="aspect-square object-cover" src="/jaimefm-3.jpeg" alt="" />
+        </div>
+        <div className="bg-gray-700 size-32">
+          <img className="aspect-square object-cover" src="/lapicara-web.png" alt="" />
+        </div>
+        <div className="bg-gray-700 size-32">
+          <img className="aspect-square object-cover" src="/testing-image.jpg" alt="" />
+        </div>
         <div className="bg-gray-700 size-32"></div>
         <div className="bg-gray-700 size-32"></div>
         <div className="bg-gray-700 size-32"></div>
@@ -24,6 +31,7 @@ export default function Home() {
       </div>
 
       <button className="mt-6 p-2 border">Añadir</button>
+      <Link href={`https://api.instagram.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_ID}&redirect_uri=${'https://ig-planner-beta.vercel.app/'}&scope=${'user_profile,user_media'}&response_type=code`}>Instagram</Link>
     </main>
   );
 }
